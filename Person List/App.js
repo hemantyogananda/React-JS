46bf2648768e5d0a53f12486c00e42be8c1e260e
hemantyogananda <hemant123yoga@gmail.com>
2020-05-06 11:09:21 +0530

    App.js

---
+++ Person List/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+
+const App = () =>(<PersonList/>)
+
+const PersonList = () =>{
+
+  const people = [
+    {
+      img:22,name:'Mukesh',job:'developer'
+    },
+    {
+      img:34,name:'Anil',job:'designer'
+    },
+    {
+      img:56,name:'Dinesh',job:'Manager'
+    }
+  ]
+  return(
+    <section>
+      <Person person={people[0]}/>
+      <Person person={people[1]}>
+        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+      </Person>
+      <Person person={people[2]}/>
+    </section>
+  )
+}
+
+const Person = (props) =>{
+  const{img,name,job} = props.person;
+  const {children} = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (<div className="person">
+    <img src={url} alt=""></img>
+    <div>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </div>
+  </div>)
+}
+
+
+export default App;
